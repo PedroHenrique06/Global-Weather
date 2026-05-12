8db962b1cfd6fd1cee2084664aa26a9affc0d1a0
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-11 22:28:31 -0300

    feat: adição do corpo da função de pesquisa e da lógica de controle de estados.

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import "./SearchBar.css";
 import lupa_icone from "../../assets/lupa-icon.png";
 
 export function SearchBar() {
+    const [searchedCityName, setSearchedCityName] = useState<string>("");
+
+    function searchForTheCityData(cityName: string) {
+        /* TODO: aciona a busca pelos dados da cidade */
+        return cityName;
+    }
+
     return(
         <>
         <div className="container-search-bar">
-            <input id="searchBarInput" className="search-bar-input" type="text" placeholder="Pesquise uma cidade..." />
-            <button className="search-button">
+            <input 
+                id="searchBarInput"
+                className="search-bar-input"
+                type="text"
+                onChange={(event) => setSearchedCityName(event.target.value)}
+                placeholder="Pesquise uma cidade..."
+                />
+            <button 
+                className="search-button"
+                onClick={() => searchForTheCityData(searchedCityName)}
+                >
                 <img className="search-button-image" src={lupa_icone} alt="Icone de lupa" />
             </button>
         </div>
