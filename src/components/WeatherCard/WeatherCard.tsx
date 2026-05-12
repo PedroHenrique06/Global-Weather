d3ba9d8666d137145a8eb205c8f10342b3ecbbec
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-12 19:29:22 -0300

    feat: adição das props (parâmetros de um componente) do card de clima.

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -4,7 +4,21 @@ import weather_icon from "../../assets/weatherCard/cloudy-sun-icon.png";
 import star_filled_icon from "../../assets/weatherCard/star-filled-icon.png";
 import star_empty_icon from "../../assets/weatherCard/star-empty-icon.png";
 
-export function WeatherCard() {
+/**
+ * @description Propriedades do componente WeatherCard.
+ */
+type weatherCardProps = {
+    cityName?: string,
+    countryName?: string,
+    weatherIcon?: string,
+    cityTemperature?: string,
+    realFeel?: string
+}
+
+/**
+ * @description Componente representando o card com as informações sobre a cidade, país, clima, ícone correspondente e sensação térmica.
+*/
+export function WeatherCard({ cityName="Mossoró", countryName="Brasil", weatherIcon=weather_icon, cityTemperature="30", realFeel="32" }:weatherCardProps) {
     const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
     function toggleFavoriteCard(){
@@ -16,8 +30,8 @@ export function WeatherCard() {
             <div className="container-weather-card">
 
                 <div className="container-city-country">
-                    <span className="card-city-text">Natal</span>
-                    <span className="card-country-text">Brasil</span>
+                    <span className="card-city-text">{cityName}</span>
+                    <span className="card-country-text">{countryName}</span>
                 </div>
 
                 <div className="container-favorite-icon-image">
@@ -30,12 +44,12 @@ export function WeatherCard() {
                 </div>
                 
                 <div className="container-icon-temperature">
-                    <img className="card-icon-image" src={weather_icon} alt="" />
-                    <div className="card-temperature-text">28°C</div>
+                    <img className="card-icon-image" src={weatherIcon} alt="" />
+                    <div className="card-temperature-text">{cityTemperature}°C</div>
                 </div>
 
                 <div className="container-real-feel">
-                    <span className="card-real-feel-text">Sensação Térmica 33°C</span>
+                    <span className="card-real-feel-text">Sensação Térmica {realFeel}°C</span>
                 </div>
 
             </div>
