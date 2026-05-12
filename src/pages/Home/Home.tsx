5457da8b56669f0ed4661c035bec1952a86bdd60
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-11 23:23:04 -0300

    feat: adição do componente que representa a seção de favoritos.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { FavoriteSession } from "../../components/FavoriteSession/FavoriteSession";
 import { RecentSession } from "../../components/RecentSession/RecentSession";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import "./Home.css";
@@ -5,10 +6,11 @@ import "./Home.css";
 export function Home() {
     return(
         <>
-        <div className="container-home">
-            <SearchBar></SearchBar>
-            <RecentSession></RecentSession>
-        </div>
+            <div className="container-home">
+                <SearchBar></SearchBar>
+                <RecentSession></RecentSession>
+                <FavoriteSession></FavoriteSession>
+            </div>
         </>
     );
 }
