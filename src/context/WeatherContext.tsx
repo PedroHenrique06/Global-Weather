e73d64f74e3f7de561bc812fbd780b4bef467c5a
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-17 21:59:30 -0300

    feat: adição das props de 'loading' e 'error'.

--- src/context/WeatherContext.tsx
+++ src/context/WeatherContext.tsx
@@ -4,6 +4,8 @@ import { type IWeatherResponse } from "../interfaces/weather";
 
 interface IWeatherContext {
     weatherList: IWeatherResponse[];
+    loading: boolean;
+    error: string;
 };
 
 export const WeatherContext = createContext<IWeatherContext | null>(null);
