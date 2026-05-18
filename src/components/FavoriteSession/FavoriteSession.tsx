41cad9c67a62638484e337fea229d4303ab63773
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-17 23:27:50 -0300

    feat: adição de dinâmização e controle de estados.

--- src/components/FavoriteSession/FavoriteSession.tsx
+++ src/components/FavoriteSession/FavoriteSession.tsx
@@ -1,7 +1,19 @@
+import { useFavoriteSession } from "../../hooks/useFavoriteSession";
 import { WeatherCard } from "../WeatherCard/WeatherCard";
 import "./FavoriteSession.css";
 
 export function FavoriteSession() {
+    const { favoriteList, loading, error } = useFavoriteSession();
+
+    // Será melhorado futuramente.
+    if(loading) {
+        return <p>Carregando...</p>;   
+    }
+
+    // Será melhorado futuramente.
+    if(error) {
+        return <p>{ error }</p>;
+    }
 
     return(
         <>
@@ -10,9 +22,17 @@ export function FavoriteSession() {
                     Favorites
                 </h2>
                 <div className="row-favorites-cards">
-                    <WeatherCard/>
-                    <WeatherCard/>
-                    <WeatherCard/>
+                    {
+                        favoriteList.map((weatherData) => (
+                            <WeatherCard
+                                key={weatherData.id}
+                                cityName={weatherData.name}
+                                cityTemperature={weatherData.main.temp}
+                                countryName={weatherData.sys.country}
+                                feelsLike={weatherData.main.feels_like}
+                            />
+                        ))
+                    }
                 </div>
             </div>
         </>
