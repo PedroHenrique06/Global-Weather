2002c06e28947095b149283b4f96357ce70a7af6
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-17 23:23:26 -0300

    feat: adição de envelopador de contexto para a seção de favoritos.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { WeatherProvider } from './context/weatherProvider.tsx'
+import { WeatherProvider } from './context/WeatherProvider.tsx'
+import { FavoriteSessionProvider } from './context/FavoriteSessionProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <WeatherProvider>
-        <App />
-    </WeatherProvider>
+    <FavoriteSessionProvider>
+        <WeatherProvider>
+            <App />
+        </WeatherProvider>
+    </FavoriteSessionProvider>
 )
