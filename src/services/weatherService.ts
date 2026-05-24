5308b4797a39e5e903bd74bc9fb2c944fdbbdc28
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-24 15:26:32 -0300

    feat: adição de tipagem a função principal, a requisição da api e utilização do mapper no retorno.

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,9 +1,11 @@
+import type { IWeatherCardInfo, IWeatherResponse } from "../interfaces/weather";
+import { mapWeatherResponseToCardInfo } from "../mappers/weatherMapper";
 import { api } from "./api";
 
-export async function getWeatherByCityName(cityName: string) {
+export async function getWeatherByCityName(cityName: string):Promise<IWeatherCardInfo> {
 
     try {
-        const response = await api.get('/weather', {
+        const response = await api.get<IWeatherResponse>('/weather', {
         params: {
             q: cityName,
             appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
@@ -12,7 +14,7 @@ export async function getWeatherByCityName(cityName: string) {
         }
         });
         
-        return response.data;
+        return mapWeatherResponseToCardInfo(response.data);
     }
     catch(error) {
         console.error(error);
