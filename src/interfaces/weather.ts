bf47be9ee94f5269d16ee31d79266ffbf1618038
PedroHenrique06 <pedrosales2806@gmail.com>
2026-05-24 13:44:15 -0300

    feat: adição de nova interface representando os dados de um card e adição de documentação.

--- src/interfaces/weather.ts
+++ src/interfaces/weather.ts
@@ -3,21 +3,46 @@
  * @description Representa a estrutura dos dados de interesse retornados pela API.
  */
 export interface IWeatherResponse {
+    /** Identificador único */
     id: number;
+
+    /** Nome da cidade */
     name: string;
     
     main: {
+        /** Temperatura */
         temp: number;
+
+        /** Sensação térmica */
         feels_like: number;
     };
 
     sys: {
+        /** Abreviação do nome do país (ex: BR) */
         country: string;
     };
 
     weather: {
+        /** Código do ícone representando o clima */
         icon: string;
+
+        /** Descrição da condição climática */
         description: string;
+
+        /** Condição climática (ex: ensolarado) */
         main: string;
     }[];
 };
+
+/**
+ * @description Representa as informações climáticas de uma cidade. 
+ */
+export interface IWeatherCardInfo {
+    id: number;
+    cityName: string;
+    countryName: string;
+    temperature: number;
+    feelsLike: number;
+    weatherStatus: string;
+    weatherDescription: string; 
+}
