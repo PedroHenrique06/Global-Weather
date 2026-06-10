a28cbd4b8a23b1c248d462be79fcb0778aa3373d
PedroHenrique06 <pedrosales2806@gmail.com>
2026-06-10 18:00:16 -0300

    feat: adição da chamada da função de pesquisa das informações da cidade.

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,32 +1,29 @@
 import { useState } from "react";
 import "./SearchBar.css";
 import lupa_icone from "../../assets/lupa-icon.png";
+import { useSearchBar } from "../../hooks/useSearchBar";
 
 export function SearchBar() {
     const [searchedCityName, setSearchedCityName] = useState<string>("");
-
-    function searchForTheCityData(cityName: string) {
-        /* TODO: aciona a busca pelos dados da cidade */
-        return cityName;
-    }
-
+    const { searchResult, searchCityInformationByName} = useSearchBar();
+    
     return(
         <>
-        <div className="container-search-bar">
-            <input 
-                id="searchBarInput"
-                className="search-bar-input"
-                type="text"
-                onChange={(event) => setSearchedCityName(event.target.value)}
-                placeholder="Pesquise uma cidade..."
-                />
-            <button 
-                className="search-button"
-                onClick={() => searchForTheCityData(searchedCityName)}
-                >
-                <img className="search-button-image" src={lupa_icone} alt="Icone de lupa" />
-            </button>
-        </div>
+            <div className="container-search-bar">
+                <input 
+                    id="searchBarInput"
+                    className="search-bar-input"
+                    type="text"
+                    onChange={(event) => setSearchedCityName(event.target.value)}
+                    placeholder="Pesquise uma cidade..."
+                    />
+                <button 
+                    className="search-button"
+                    onClick={() => { searchCityInformationByName(searchedCityName); console.log(`${searchedCityName}: ${searchResult.cityName} - ${searchResult.countryName} - ${searchResult.temperature} `)}}
+                    >
+                    <img className="search-button-image" src={lupa_icone} alt="Icone de lupa" />
+                </button>
+            </div>
         </>
     )
 }
